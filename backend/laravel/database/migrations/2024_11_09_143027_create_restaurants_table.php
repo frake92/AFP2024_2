f313978a6ab90b0f269277abc013caf2ef14fa3c
TMark-ZI3OV4 <takimark26@gmail.com>
2024-11-18 19:52:19 +0100

    Restaurants migráció javítása

--- backend/laravel/database/migrations/2024_11_09_143027_create_restaurants_table.php
+++ backend/laravel/database/migrations/2024_11_09_143027_create_restaurants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 255);
             $table->string('address', 255);
-            $table->string('phone', 15);
+            $table->string('phone', 50);
             $table->text('description')->nullable();
             $table->string('picture_path', 255)->nullable();
             $table->timestamps();
