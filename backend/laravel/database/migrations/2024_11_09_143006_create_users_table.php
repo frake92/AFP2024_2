767d3aa000534ab7cf70309e9af1fb20da2a5b13
TMark-ZI3OV4 <takimark26@gmail.com>
2024-11-20 13:53:20 +0100

    conflict kezelés

--- backend/laravel/database/migrations/2024_11_09_143006_create_users_table.php
+++ backend/laravel/database/migrations/2024_11_09_143006_create_users_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
             $table->timestamps();
         });
     }
